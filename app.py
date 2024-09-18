19efe6cc23753f3a96a176147a5ace213c1743bd
Brunno Bronner <116854914+BruBronner@users.noreply.github.com>
2024-09-18 13:45:20 -0300

    Aprimorando o código

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
 from deep_translator import GoogleTranslator  
-import PySimpleGUI as sg   
+import PySimpleGUI as sg  
 
-sg.theme('Topanga')  
 
-tradutor = GoogleTranslator(source='portuguese', target="en")  
+sg.theme('DarkGreen7')
+layout = [  
+    [sg.Text("Tradutor", font=("Helvetica", 16), justification='center', pad=(10, 10))],  
+    [sg.Text("Digite a frase:", font=("Helvetica", 12), pad=(10, 5))],  
+    [sg.InputText('', key='-TEXTO-', size=(50, 1), pad=(10, 5))],  
+    [sg.Button('Traduzir', size=(10, 1), pad=(10, 5)), sg.Button('Sair', size=(10, 1), key='-SAIR', pad=(10, 5))],  
+    [sg.Text('Tradução:', font=("Helvetica", 12), pad=(10, 5)), sg.Text(size=(40, 1), key='-TRADUCAO-', font=("Helvetica", 12), pad=(10, 5))]  
+]  
+window = sg.Window("Tradutor", layout, element_justification='c', finalize=True)  
 
-layout = [ 
-    [sg.Text("Translate")],  
-    [sg.Text("Texto para traduzir:", size=(15, 1)), sg.InputText(key='-TEXTO-')],  
-    [sg.Button('Traduzir'), sg.Button('Sair', key='-SAIR')],  
-    [sg.Text('Tradução:', size=(15, 1)), sg.Text(size=(40, 1), key='-TRADUCAO-')] 
-]
 
-window = sg.Window('Tradutor', layout) 
-
+def traduzir(texto):  
+    try:  
+        if texto.strip():  # Verifica se o texto não está vazio  
+            return GoogleTranslator(source='auto', target='pt').translate(texto)  
+        return "Por favor, insira um texto para traduzir."  
+    except Exception as e:  
+        return "Erro na tradução: " + str(e)  
 
 while True:  
     event, values = window.read()  
-    
-    if event == sg.WIN_CLOSED or event == "-SAIR":  
+    if event in (sg.WIN_CLOSED, '-SAIR'):  
         break  
-    
     if event == 'Traduzir':  
         texto = values['-TEXTO-']  
-        
-        try:  
-            traducao = tradutor.translate(texto)  
-            window['-TRADUCAO-'].update(traducao)  
-            window['-TEXTO-'].update('')  # Limpa o campo de entrada  
-        except Exception as e:  
-            window['-TRADUCAO-'].update(f"Erro na tradução: {str(e)}")  
-    
+        traducao = traduzir(texto)  
+        window['-TRADUCAO-'].update(traducao)  
+
+
 window.close()  
